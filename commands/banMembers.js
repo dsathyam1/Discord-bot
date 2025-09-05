b8fbfb7a87023f4c4782f199c168dbdd9640615a
dsathyam1 <dahalsathyam@gmail.com>
2025-09-05 13:16:30 +0545

    formated code

--- commands/banMembers.js
+++ commands/banMembers.js
@@ -1,27 +1,32 @@
 module.exports = {
-    name: "!ban",
-    description: "This command will ban memebers, but you need the adminstration permission", 
-    async execute(msg){
-        if(!msg.member.permissions.has("BanMembers")) {
-            return msg.reply(
-                "You do not have permission to ban members. (Adminstration permission required)"
-            ); 
-        }
-        const user = msg.mentions.members.first(); 
-        if(!user){
-            return msg.reply("You need to mention a member to ban. Example: `!ban @user` "); 
-        }
-    
-        if(!user.bannable){
-            return msg.reply("I cannot ban this member. They may have higher permissions than me"); 
-        }
+  name: "!ban",
+  description:
+    "This command will ban memebers, but you need the adminstration permission",
+  async execute(msg) {
+    if (!msg.member.permissions.has("BanMembers")) {
+      return msg.reply(
+        "You do not have permission to ban members. (Adminstration permission required)"
+      );
+    }
+    const user = msg.mentions.members.first();
+    if (!user) {
+      return msg.reply(
+        "You need to mention a member to ban. Example: `!ban @user` "
+      );
+    }
 
-        try{
-            await user.ban();
-            msg.channel.send(`${user.user.tag} has been banned from the server`); 
-        }catch(err){
-            console.error(err);
-            msg.reply("There was something wrong trying ban this member."); 
-        }
-    },
+    if (!user.bannable) {
+      return msg.reply(
+        "I cannot ban this member. They may have higher permissions than me"
+      );
+    }
+
+    try {
+      await user.ban();
+      msg.channel.send(`${user.user.tag} has been banned from the server`);
+    } catch (err) {
+      console.error(err);
+      msg.reply("There was something wrong trying ban this member.");
+    }
+  },
 };
